7fc60573d48097ec6630f80533facf019b0db513
hemabouts-cmd <hemabouts@gmail.com>
2025-09-05 20:03:01 +0200

    Create App.js

---
+++ App.js
@@ -0,0 +1,12 @@
+{
+  "expo": {
+    "name": "Histoires du soir",
+    "slug": "histoires-du-soir",
+    "version": "1.0.0",
+    "orientation": "portrait",
+    "scheme": "histoiressoir",
+    "assetBundlePatterns": ["**/*"],
+    "ios": { "bundleIdentifier": "com.tonnom.histoiressoir", "supportsTablet": true },
+    "android": { "package": "com.tonnom.histoiressoir" }
+  }
+}
